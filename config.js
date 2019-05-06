d77caac9c9373e7926cadd49633c1d4296569c12
Mariusz Mycek <m.mycek83@gmail.com>
2019-05-06 13:34:02 +0200

    Finish simple Google authentication app

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID': '540671357846-q5aargum0k1psibkba9mj21p9istmau1.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET': 'axd3We2GGX2BK6nR7YUkegur',
+  'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
